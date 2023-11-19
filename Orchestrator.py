d91f0292e220b6d55a92e415a707ebd22b4a6f9c
SinkableVirus <srini22003@gmail.com>
2023-11-19 14:56:53 +0530

    heartbeat

--- Orchestrator.py
+++ Orchestrator.py
@@ -1,5 +1,6 @@
 import json
 import random
+import time
 from kafka import KafkaProducer
 from kafka import KafkaConsumer
 from apscheduler.schedulers.background import BackgroundScheduler
@@ -8,10 +9,13 @@ from apscheduler.schedulers.background import BackgroundScheduler
 class OrchestratorNode:
     def __init__(self):
         self.metrics = {}
+        self.heartbeat = {}
         self.producer = KafkaProducer(bootstrap_servers = "localhost:9092")
         self.consumer = KafkaConsumer("register", "metrics", "heartbeat")
         self.scheduler = BackgroundScheduler()
         self.scheduler.start()
+        self.scheduler2 = BackgroundScheduler()
+        self.scheduler2.start()
 
     def testConfig(self):
         self.testID = random.randint(0, 100)
@@ -42,6 +46,14 @@ class OrchestratorNode:
 
     def printMetrics(self):
         print(json.dumps(self.metrics, indent = 4))
+    
+    def checkHeartbeat(self):
+        for i in self.heartbeat:
+            if(time.time() - self.heartbeat[i] > 1.5):
+                print(f'Heartbeat from node {i} not received')
+            else:
+                print(f'Heartbeat from node {i} received')
+
 
     def listen(self):
         for message in self.consumer:
@@ -52,10 +64,24 @@ class OrchestratorNode:
             if message.topic == "metrics":
                 self.metrics[data["node_id"]] = data["metrics"]
 
+            if message.topic == "heartbeat":
+                # if data["node_id"] not in self.checkHeartbeat:
+                self.heartbeat[data["node_id"]] = time.time()
+                # else:
+                #     if (time.time() - self.checkHeartbeat[data["node_id"]]) > 1.5:
+                #         print(f'Received heartbeat from node {data["node_id"]} late')
+                #     else:
+                #         print(f'Received heartbeat from node {data["node_id"]} on time')
+                #     self.checkHeartbeat[data["node_id"]] = time.time()
+
+                
+
+
 
 if __name__ == "__main__":
     orchestrator = OrchestratorNode()
     orchestrator.testConfig()
     orchestrator.trigger()
     orchestrator.scheduler.add_job(orchestrator.printMetrics, 'interval', seconds = 1)
+    orchestrator.scheduler2.add_job(orchestrator.checkHeartbeat, 'interval', seconds = 1)
     orchestrator.listen()
